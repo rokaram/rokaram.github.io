ae2181dfb0c4bda4041164f886795d07b985db99
Karamov Ramazan <karamovvomarak@gmail.com>
2020-03-17 00:14:58 +0500

    better clicker

--- clicker/script.js
+++ clicker/script.js
@@ -5,15 +5,100 @@ const countMoneyOut = document.querySelector('.gameplay__header-money');
 const powerClickOut = document.querySelector('.gameplay__header-force');
 const btnClick = document.querySelector('.gameplay__main-btn');
 
-let marketItemAll = document.querySelectorAll('.market__item');
+const productFunctions = [
+    {
+        productFunctionUseful: function(i) {     
+            productInfo[i].productPrice *= 2;
+            console.log(productInfo)
+            marketItemPrices[i].innerHTML = `${productInfo[i].productPrice}$`;
+            localStorage.setItem('pluser', ++pluser)
+            powerClickOut.innerHTML = `Сила: ${pluser}`;
+        }
+    },
+    {
+        productFunctionUseful: function(i) {
+            localStorage.setItem('clicksForActiveProduct', ++clicksForActiveProduct);
 
-let isPowerPlus = false;
-let isAutoClick = false;
+            if(clicksForActiveProduct == 1) {
+                let autoClickLogic = setInterval(() => {
+                    currentClicks--;
+                    btnClick.dispatchEvent( new Event('click') );
+                    localStorage.setItem('timeForAutoClick', ++timeForAutoClick);
+                    
+                    if(timeForAutoClick >= productInfo[i].productUseful) {
+                        clearInterval(autoClickLogic);
+                        localStorage.setItem('timeForAutoClick', timeForAutoClick -= timeForAutoClick);
+                        localStorage.setItem('clicksForActiveProduct', clicksForActiveProduct = 0);
+                    }
+                }, 1000);
+            }
+        }
+    }
+]
+
+// draw products
+const market = document.querySelector('.market__inner');
+let timeForAutoClick = localStorage.getItem('timeForAutoClick');
+let clicksForActiveProduct = localStorage.getItem('clicksForActiveProduct');
+
+let productInfo = JSON.parse(localStorage.getItem('productInfo'));
+
+if (!productInfo) {
+    localStorage.setItem('productInfo', JSON.stringify([   
+        {   
+            productName: `+2 к силе Кликера`,
+            productUseful: 1,
+            productPrice: 20,
+        },
+        {
+            productName: `Автоклик в течений 30 сек`,
+            productUseful: 30,
+            productPrice: '',
+        },
+        {
+            productUseful: 'unknown',
+            productName: 'unknown',
+            productPrice: 'unknown'
+        }
+    ]));
+
+    productInfo = JSON.parse(localStorage.getItem('productInfo'));
+};
+
+
+for(let i = 0; i < productInfo.length; i++) {
+    market.innerHTML += `
+        <div class="market__container">
+            <div class="market__item">
+                <p class="market__item-name">${productInfo[i].productName}</p>
+                <p class="market__item-price">${productInfo[i].productPrice}$</p>
+            </div>
+        </div>
+    `
+}
+
+// logic for market
+const marketItems = document.querySelectorAll('.market__item');
+const marketItemPrices = document.querySelectorAll('.market__item-price');
+
+for(let i = 0; i < marketItems.length; i++) {
+    marketItems[i].addEventListener('click', function() {
+        if (countMoney >= productInfo[i].productPrice) { 
+            saveMoney(countMoney -= productInfo[i].productPrice);
+            countMoneyOut.innerHTML = `${countMoney}$`;
+            productFunctions[i].productFunctionUseful(i);
+            localStorage.setItem('productInfo', JSON.stringify(productInfo));
+        } else {
+            alert('nope')
+        }
+    });
+}
+
 
 let currentClicks = localStorage.getItem('currentClicks');
 let countMoney = localStorage.getItem('countMoney');
-let powerClick = 0;
 let pluser = localStorage.getItem('pluser');
+
 
 // information output if this new game
 if( !pluser ) {
@@ -56,70 +141,6 @@ window.addEventListener('keydown', ({ key }) => {
         localStorage.clear();
     }
 });
-
-
-// draw products
-const market = document.querySelector('.market__inner');
-let timeForAutoClick = localStorage.getItem('timeForAutoClick');
-const productInfo = [
-    {   
-        productUseful: 1,
-        productName: `+2 к силе Кликера`,
-        productPrice: 20,
-        productFunctionUseful: function() {
-            localStorage.setItem('pluser', ++pluser)
-            powerClickOut.innerHTML = `Сила: ${pluser}`;
-        }
-    },
-    {
-        productUseful: 10,
-        productName: `Автоклик в течений 1 мин`,
-        productPrice: 0,
-        productFunctionUseful: function() {
-            let autoClickLogic = setInterval(() => {
-                currentClicks--;
-                btnClick.dispatchEvent( new Event('click') );
-                localStorage.setItem('timeForAutoClick', ++timeForAutoClick);
-                
-                if(timeForAutoClick >= 10) {
-                    clearInterval(autoClickLogic);
-                    localStorage.setItem('timeForAutoClick', timeForAutoClick -= timeForAutoClick);
-                }
-            }, 1000)
-        }
-    },
-    {
-        productUseful: 'unknown',
-        productName: 'unknown',
-        productPrice: 'unknown'
-    }
-];
-
-for(let i in productInfo) {
-    market.innerHTML += `
-        <div class="market__container">
-            <div class="market__item">
-                <p class="market__item-name">${productInfo[i].productName}</p>
-                <p class="market__item-price">${productInfo[i].productPrice}$</p>
-            </div>
-        </div>
-    `
-}
-
-
-// logic for market
-marketItemAll = document.querySelectorAll('.market__item');
-for(let i = 0; i < marketItemAll.length; i++) {
-    marketItemAll[i].addEventListener('click', function() {
-        if (countMoney >= productInfo[i].productPrice) { 
-            saveMoney(countMoney -= productInfo[i].productPrice);
-            countMoneyOut.innerHTML = `${countMoney}$`;
-            productInfo[i].productFunctionUseful();
-        } else {
-            alert('nope')
-        }
-    });
-}
 
 
 // continue auto click after page reload
