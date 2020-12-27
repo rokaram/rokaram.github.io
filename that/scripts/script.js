910ab6013d1bf45e60e0b180e83a9cd16ce7939b
Karamov Ramazan <karamovvomarak@gmail.com>
2020-12-28 00:04:32 +0500

    that update

--- that/scripts/script.js
+++ that/scripts/script.js
@@ -3,9 +3,16 @@
 const colors = ['#DC143C', '#C71585', '#FFD700', '#FF8C00', '#EE82EE', '#32CD32']
 const text = document.querySelector('.intro__title')
 const updateBtn = document.querySelector('.intro__updateBtn')
+const introBlock = document.querySelector('.intro__block')
+const titleAnim = 'headShake'
 
 const rand = (min, max) => {
     return Math.round(Math.random() * (max - min) + min)
+}
+
+const anim = (htmlEl, animName) => {
+    htmlEl.classList.remove(animName, 'animated')
+    setTimeout(() => htmlEl.classList.add(animName, 'animated'), 0)
 }
 
 const splitClass = (htmlEl, className) => {
@@ -49,6 +56,8 @@ multicolsizeLet(text)
 
 updateBtn.addEventListener('click', () => {
     multicolsizeLet(text)
+    anim(text, titleAnim)
+    anim(updateBtn, 'jello')
 })
 
 const textLets = document.querySelectorAll('.intro__title-letter')
@@ -56,4 +65,12 @@ const textLets = document.querySelectorAll('.intro__title-letter')
 textLets.forEach(el => {
     el.addEventListener('mouseover', () => multicolsizeLet(el))
     el.addEventListener('touchmove', () => multicolsizeLet(el))
+})
+
+introBlock.addEventListener('mouseover', () => {
+    text.classList.add('tada', 'animated')
+})
+
+introBlock.addEventListener('mouseout', () => {
+    text.classList.remove('tada', 'animated')
 })
