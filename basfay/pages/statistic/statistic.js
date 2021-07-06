756285057cd8af7f89e18d5de799fc5e340e314c
rokaram <karamovvomarak@gmail.com>
2021-07-06 15:45:18 +0300

    update basfay

---
+++ basfay/pages/statistic/statistic.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const collectedScoreOut = document.querySelector('.collectedScore')
+const playedMatchesOut = document.querySelector('.matchesPlayed')
+const timeInGameOut = document.querySelector('.timeInGame')
+const bestScoreOut = document.querySelector('.bestScore')
+
+let allScores = JSON.parse(localStorage.getItem('allScores')) || []
+let timeInGame = localStorage.getItem('timeInGame') || 0
+let playedMatches = localStorage.getItem('playedMatches') || 0
+let collectedScoresSum = allScores.length && allScores.map(el => el.score).reduce((el, prev) => +el + +prev)
+let bestScore = allScores.map(el => el.score).sort((a, b) => a - b)[allScores.length - 1] || 0
+
+collectedScoreOut.textContent = collectedScoresSum
+playedMatchesOut.textContent = playedMatches
+bestScoreOut.textContent = bestScore
+timeInGameOut.innerText = timeInGame > 60 ? `${Math.round(timeInGame / 60)} мин` : `${timeInGame} секунд`
